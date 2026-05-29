394cc412df8dee1eee7eb3475e6614472a016608
auraaa0 <auraramadhani@student.telkomuniversity.ac.id>
2026-05-29 16:05:07 +0700

    Add files via upload

--- routes/routes.go
+++ routes/routes.go
@@ -1,68 +1,52 @@
-// Author: Hanif
-// PBI: KF-13
-// Sprint: Sprint 1
-package routes
-
-import (
-	"marilancy/controllers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func SetupRoutes(r *gin.Engine) {
-	// Author: Hanif
-	// PBI: KF-13
+	// Author: Aura
+	// PBI: KF-11
 	// Sprint: Sprint 1
-	r.GET("/admin", func(c *gin.Context) {
-		c.HTML(200, "dashboard_admin.html", nil)
+	r.GET("/jobs/:id", controllers.GetJobDetail)
+
+	// Author: Aura
+	// PBI: KF-15
+	// Sprint: Sprint 2
+	r.GET("/freelancer/:id/rating-summary", controllers.GetFreelancerRatingSummary)
+
+	// Author: Aura
+	// PBI: KF-15
+	// Sprint: Sprint 2
+	r.GET("/client/rating", func(c *gin.Context) {
+		c.HTML(200, "rating.html", nil)
 	})
 
-	// Author: Hanif
-	// PBI: KF-13
+	// Author: Aura
+	// PBI: KF-11
 	// Sprint: Sprint 1
-	r.GET("/client", func(c *gin.Context) {
-		c.HTML(200, "dashboard_client.html", nil)
+	r.GET("/job/detail", func(c *gin.Context) {
+		c.HTML(200, "job_detail.html", nil)
 	})
 
-	// Author: Hanif
-	// PBI: KF-13
+	// Author: Aura
+	// PBI: KF-07
 	// Sprint: Sprint 1
-	r.GET("/freelancer", func(c *gin.Context) {
-		c.HTML(200, "dashboard_freelancer.html", nil)
+	r.GET("/client/saved-applicants", func(c *gin.Context) {
+		c.HTML(200, "saved-applicants.html", nil)
 	})
 
-	// Author: Fadhil
-	// PBI: KF-02
+	// Author: Aura
+	// PBI: KF-03
 	// Sprint: Sprint 1
-	r.GET("/client/profile/view", func(c *gin.Context) {
-		c.HTML(200, "profile.html", nil)
-	})
+	client := r.Group("/client")
+	client.Use(
+		middleware.AuthMiddleware(),
+		middleware.RoleMiddleware("client"),
+	)
+	{
+		client.GET("/profile", controllers.GetClientProfile)
+		client.PUT("/profile", controllers.UpdateClientProfile)
+		client.POST("/jobs", controllers.CreateJob)
+		client.PUT("/jobs/:id", controllers.UpdateJob)
+		client.GET("/jobs/:id/applicants", controllers.GetJobApplicants)
+		client.PUT("/application/status", controllers.UpdateApplicationStatus)
+		client.DELETE("/jobs/:id", controllers.DeleteJob)
+		client.GET("/my-projects", controllers.GetClientProjects)
+		client.POST("/rating", controllers.CreateRating)
+		client.GET("/check-rating", controllers.CheckRating)
+	}
 
-	// Author: Fadhil
-	// PBI: KF-02
-	// Sprint: Sprint 1
-	r.GET("/freelancer/profile/view", func(c *gin.Context) {
-		c.HTML(200, "profilefree.html", nil)
-	})
-
-	// Author: Arga
-	// PBI: KF-17
-	// Sprint: Sprint 1
-	r.GET("/admin/data", controllers.AdminDashboardData)
-	r.GET("/admin/freelancers", controllers.GetFreelancers)
-	r.GET("/admin/clients", controllers.GetClients)
-	r.DELETE("/admin/freelancers/:id", controllers.DeleteFreelancer)
-	r.DELETE("/admin/clients/:id", controllers.DeleteClient)
-
-	// Author: Fadhil
-	// PBI: KF-02
-	// Sprint: Sprint 1
-	r.GET("/client/profile", controllers.GetClientProfile)
-	r.PUT("/client/profile", controllers.UpdateClientProfile)
-
-	// Author: Fadhil
-	// PBI: KF-02
-	// Sprint: Sprint 1
-	r.GET("/freelancer/profile", controllers.GetFreelancerProfile)
-	r.PUT("/freelancer/profile", controllers.UpdateFreelancerProfile)
-}
